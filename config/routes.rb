7f68c32feed6b4a9377ed0450c636b91b8c51001
robin-hassan <robiul.hassan12102@gmail.com>
2020-09-26 21:55:49 +0800

    added user basic auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   scope :api do
     scope :v1 do
+      resources :users, only: [:create]
+      post '/login', to: 'users#login'
       resources :posts
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_25_193330) do
+ActiveRecord::Schema.define(version: 2020_09_26_134638) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.string "first_name"
+    t.string "last_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
